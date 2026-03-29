dff20117cd58720ef17816ed153503ca1a0b3768
VictoriaGrageda <victoriagragedavallejos@gmail.com>
2026-03-29 18:54:49 -0400

    Implementación de registro de red profesional con validaciones

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import UserHome from '@/pages/UserHome'
 import UserPersonalData from "@/pages/UserPersonalData";
+import NetworksPage from "@/pages/NetworksPage";
 import ProfilePage from "../pages/ProfilePage";
 
 function AppRoutes() {
@@ -9,6 +10,7 @@ function AppRoutes() {
       <Route path="/" element={<UserHome />} />
       <Route path="/dashboard" element={<UserHome />} />
       <Route path="/personal" element={<UserPersonalData />} />
+      <Route path="/red-profesional" element={<NetworksPage />} />
       <Route path="/perfil" element={<ProfilePage />} />
     </Routes>
   )
