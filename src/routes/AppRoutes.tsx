d5cb5eca2779783888c3a271ab5102380058b7dc
Jahzeel <jahzeelcarlo1@gmail.com>
2026-03-30 19:21:25 -0400

    Merge completo

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -7,15 +7,10 @@ import RegisterPage from '../pages/RegisterPage'
 import UserHome from '@/pages/UserHome'
 import UserPersonalData from "@/pages/UserPersonalData";
 import ProfilePage from "../pages/ProfilePage";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import CreateProyect from '../pages/CreateProyect';
-=======
 import UserSkills from "@/pages/UserSkills";
->>>>>>> 46a10fa7d79558f1c407ddab3f9259abc432aae2
-=======
-import ExperiencePage from "@/pages/ExperiencePage";
->>>>>>> 3fbf0f81a1fbdeb04e3466d6bdd802fc57f1ba4b
+import NetworksPage from '../pages/NetworksPage';
+import ExperiencePage from '../pages/ExperiencePage';
 
 function AppRoutes() {
   return (
@@ -27,12 +22,11 @@ function AppRoutes() {
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard" element={<UserHome />} />
       <Route path="/personal" element={<UserPersonalData />} />
-      <Route path="/red-profesional" element={<NetworksPage />} />
-      <Route path="/experiencia" element={<ExperiencePage />} />
       <Route path="/perfil" element={<ProfilePage />} />
       <Route path="/proyectos" element={<CreateProyect />} />
-=======
       <Route path="/habilidades" element={<UserSkills />} />
+      <Route path="/red-profesional" element={<NetworksPage />} />
+      <Route path="/experiencia" element={<ExperiencePage />} />
     </Routes>
   )
 }
